4cab92fa6689ab5b19e81dc82a7891300111029d
t-manuel2095 <t.manuel2095@gmail.com>
2026-05-29 12:06:42 -0500

    Updated the pictures so they cover the blank space they had around the boarders. Also, make a header showing that I created it. Lastly, created a very short about page.

--- NFLTop100/NFLTop100/urls.py
+++ NFLTop100/NFLTop100/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from django.contrib import admin
-from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import TemplateView
 from django.conf import settings
 
@@ -8,7 +8,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('players.urls')),
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
